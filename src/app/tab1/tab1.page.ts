17134fb72dd6073fbc703d6daf783a7edd44f893
Nara Garcia <garcia.nara3@gmail.com>
2020-06-15 22:24:24 -0300

    Configurando tabs
    Inserindo imagem do qrCode

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,40 +2,64 @@ import { Component } from '@angular/core';
 import { Dialogs } from '@ionic-native/dialogs/ngx';
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 import { Platform } from '@ionic/angular';
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
-})
+})  
 export class Tab1Page {
   qrScan: any;
 
-  constructor(public platform: Platform, public dialog: Dialogs, public qr: QRScanner) {
-    //Agora desabilitar o botão de scaner quando o botão de volta for pressionado
-    this.platform.backButton.subscribeWithPriority(0, () => {
-      document.getElementsByTagName("ion-content")[0].style.opacity = "1";
-      this.qrScan.unsubsribe();
-    })
-  }
+  public corpoPagina: HTMLElement;
+  public img: HTMLElement;
 
-  StartScanning() {
-    this.qr.prepare().then((status: QRScannerStatus) => {
+  public scanner: any;
+
+  constructor(private qrScanner: QRScanner,private dialogs: Dialogs, public platform: Platform){
+    
+    this.platform.backButton.subscribeWithPriority(0,()=>{
+      this.corpoPagina.style.opacity="1";
+      this.img.style.opacity="1";
+
+      this.qrScanner.hide();
+      this.scanner.unsubscribe();
+
+    });
+   }
+  public lerQRCode(){
+    // Optionally request the permission early
+    this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
       if (status.authorized) {
-        this.qr.show();
-        document.getElementsByTagName("ion-content")[0].style.opacity = "0";
-        this.qrScan = this.qr.scan().subscribe((textFound) => {
-          document.getElementsByTagName("ion-content")[0].style.opacity = "1";
-          this.qrScan.unsubsribe();
-          this.dialog.alert(textFound);
-        }, (err) => {
-          this.dialog.alert(JSON.stringify(err));
-        })
-      }
-      else if (status.denied) {
+        // camera permission was granted
+        this.qrScanner.show();
+        this.corpoPagina = document.getElementsByTagName('ion-content')[0] as HTMLElement;
+        this.corpoPagina.style.opacity = "0";
 
+        this.img = document.getElementById('logo') as HTMLElement;
+        this.img.style.opacity = "0";
+
+
+        // start scanning
+        this.scanner = this.qrScanner.scan().subscribe((text: string) => {
+          console.log('Scanned something', text);
+          this.dialogs.alert('Resultado: ' + text);
+          this.corpoPagina.style.opacity = "1";
+          this.img.style.opacity="1";
+
+          this.qrScanner.hide(); // hide camera preview
+          this.scanner.unsubscribe(); // stop scanning
+        });
+      } else if (status.denied) {
+        // camera permission was permanently denied
+        // you must use QRScanner.openSettings() method to guide the user to the settings page
+        // then they can grant the permission from there
+      } else {
+        // permission was denied, but not permanently. You can ask for permission again at a later time.
       }
     })
+    .catch((e: any) => console.log('Error is', e)); 
   }
-
+  ngOnInit() {
+  }
 }

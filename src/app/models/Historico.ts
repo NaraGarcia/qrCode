a8e371b44455bbbcb908fb9313590c04fec7655f
Nara Garcia <garcia.nara3@gmail.com>
2020-06-26 20:58:10 -0300

    Histórico

---
+++ src/app/models/Historico.ts
@@ -0,0 +1,5 @@
+export class Historico {  
+
+    leitura: string;
+    dataHora: string;
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Historico } from '../models/Historico';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public historicos: Historico[] = [
+  
+    {dataHora: "25/06/2020 19:00", leitura: "Leitura 01"},
+    {dataHora: "25/06/2020 19:30", leitura: "Leitura 02"},
+    {dataHora: "25/06/2020 20:00", leitura: "Leitura 03"},
+    {dataHora: "25/06/2020 20:30", leitura: "Leitura 04"},
+    {dataHora: "26/06/2020 19:40", leitura: "Leitura 05"},
+
+  ];
+
+  constructor() {
+
+
+
+  }
+
+
 
 }
